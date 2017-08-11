5e410c444610b7b234411f8741fbc9b1b8dd3110
alejandrochvs <alejandrochvs@hotmail.com>
2017-08-11 13:13:56 -0500

    Auto push V1.1.1

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,86 @@
-var _0xa6d6=["\x65\x6D\x61\x69\x6C","\x70\x68\x6F\x6E\x65","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x76\x61\x6C\x75\x65","\x74\x65\x73\x74","\x77\x72\x6F\x6E\x67","\x72\x65\x6D\x6F\x76\x65\x43\x6C\x61\x73\x73","\x23","\x61\x64\x64\x43\x6C\x61\x73\x73","\x68\x6F\x76\x65\x72","\x49\x6E\x63\x6F\x72\x72\x65\x63\x74","\x74\x6F\x70","\x74\x6F\x6F\x6C\x74\x69\x70","\x76\x61\x6C","\x23\x6E\x61\x6D\x65","\x23\x6C\x61\x73\x74\x5F\x6E\x61\x6D\x65","\x23\x65\x6D\x61\x69\x6C","\x23\x73\x74\x61\x74\x65","\x23\x70\x68\x6F\x6E\x65","\x6E\x61\x6D\x65","\x6C\x61\x73\x74\x5F\x6E\x61\x6D\x65","\x73\x74\x61\x74\x65","\x3A\x63\x68\x65\x63\x6B\x65\x64","\x69\x73","\x69\x6E\x70\x75\x74\x5B\x74\x79\x70\x65\x3D\x22\x63\x68\x65\x63\x6B\x62\x6F\x78\x22\x5D","\x6C\x61\x62\x65\x6C","\x62\x6F\x74\x74\x6F\x6D","\x50\x4F\x53\x54","\x2F\x72\x65\x67\x69\x73\x74\x65\x72","\x31\x31\x30\x30\x30","\x54\x68\x65\x20\x65\x2D\x6D\x61\x69\x6C\x20\x61\x6C\x72\x65\x61\x64\x79\x20\x65\x78\x69\x73\x74\x73\x2E","\x64\x6F\x6E\x65","\x74\x6F\x67\x67\x6C\x65\x43\x6C\x61\x73\x73","\x2E\x66\x6F\x72\x6D\x2D\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x61\x6A\x61\x78","\x6C\x6F\x67","\x63\x6C\x69\x63\x6B","\x62\x75\x74\x74\x6F\x6E","\x68\x72\x65\x66","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x6F\x66\x66\x63\x6F\x72\x73\x73\x2E\x63\x6F\x6D","\x2E\x74\x6F\x70\x2D\x6E\x61\x76\x20\x3E\x20\x2E\x6C\x6F\x67\x6F","\x41\x6C\x61\x62\x61\x6D\x61","\x41\x6C\x61\x73\x6B\x61","\x41\x72\x69\x7A\x6F\x6E\x61","\x41\x72\x6B\x61\x6E\x73\x61\x73","\x43\x61\x6C\x69\x66\x6F\x72\x6E\x69\x61","\x43\x6F\x6C\x6F\x72\x61\x64\x6F","\x43\x6F\x6E\x6E\x65\x63\x74\x69\x63\x75\x74","\x44\x65\x6C\x61\x77\x61\x72\x65","\x46\x6C\x6F\x72\x69\x64\x61","\x47\x65\x6F\x72\x67\x69\x61","\x48\x61\x77\x61\x69\x69","\x49\x64\x61\x68\x6F","\x49\x6C\x6C\x69\x6E\x6F\x69\x73","\x49\x6E\x64\x69\x61\x6E\x61","\x49\x6F\x77\x61","\x4B\x61\x6E\x73\x61\x73","\x4B\x65\x6E\x74\x75\x63\x6B\x79","\x4C\x6F\x75\x69\x73\x69\x61\x6E\x61","\x4D\x61\x69\x6E\x65","\x4D\x61\x72\x79\x6C\x61\x6E\x64","\x4D\x61\x73\x73\x61\x63\x68\x75\x73\x65\x74\x74\x73","\x4D\x69\x63\x68\x69\x67\x61\x6E","\x4D\x69\x6E\x6E\x65\x73\x6F\x74\x61","\x4D\x69\x73\x73\x69\x73\x73\x69\x70\x70\x69","\x4D\x69\x73\x73\x6F\x75\x72\x69","\x4D\x6F\x6E\x74\x61\x6E\x61","\x4E\x65\x62\x72\x61\x73\x6B\x61","\x4E\x65\x76\x61\x64\x61","\x4E\x65\x77","\x48\x61\x6D\x70\x73\x68\x69\x72\x65","\x4A\x65\x72\x73\x65\x79","\x4D\x65\x78\x69\x63\x6F","\x59\x6F\x72\x6B","\x4E\x6F\x72\x74\x68","\x43\x61\x72\x6F\x6C\x69\x6E\x61","\x44\x61\x6B\x6F\x74\x61","\x4F\x68\x69\x6F","\x4F\x6B\x6C\x61\x68\x6F\x6D\x61","\x4F\x72\x65\x67\x6F\x6E","\x50\x65\x6E\x6E\x73\x79\x6C\x76\x61\x6E\x69\x61","\x52\x68\x6F\x64\x65","\x49\x73\x6C\x61\x6E\x64","\x53\x6F\x75\x74\x68","\x54\x65\x6E\x6E\x65\x73\x73\x65\x65","\x54\x65\x78\x61\x73","\x55\x74\x61\x68","\x56\x65\x72\x6D\x6F\x6E\x74","\x56\x69\x72\x67\x69\x6E\x69\x61","\x57\x61\x73\x68\x69\x6E\x67\x74\x6F\x6E","\x57\x65\x73\x74","\x57\x69\x73\x63\x6F\x6E\x73\x69\x6E","\x57\x79\x6F\x6D\x69\x6E\x67","\x6C\x65\x6E\x67\x74\x68","\x3C\x6F\x70\x74\x69\x6F\x6E\x20\x76\x61\x6C\x75\x65\x3D\x22","\x22\x20\x63\x6C\x61\x73\x73\x3D\x22\x63\x6F\x6C\x2D\x78\x73\x2D\x31\x32\x22\x3E","\x3C\x2F\x6F\x70\x74\x69\x6F\x6E\x3E","\x61\x70\x70\x65\x6E\x64","\x73\x65\x6C\x65\x63\x74"];$(function(){function _0x8574x1(_0x8574x2){var _0x8574x3=/^[a-zA-Z ]{2,30}$/;if(_0x8574x2== _0xa6d6[0]){var _0x8574x3=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/}else {if(_0x8574x2== _0xa6d6[1]){var _0x8574x3=/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im}};var _0x8574x4=document[_0xa6d6[2]](_0x8574x2);if(_0x8574x3[_0xa6d6[4]](_0x8574x4[_0xa6d6[3]])){$(_0xa6d6[7]+ _0x8574x2)[_0xa6d6[6]](_0xa6d6[5]);return true}else {$(_0xa6d6[7]+ _0x8574x2)[_0xa6d6[8]](_0xa6d6[5]);$(_0x8574x4)[_0xa6d6[12]]({"\x74\x72\x69\x67\x67\x65\x72":_0xa6d6[9],"\x74\x69\x74\x6C\x65":_0xa6d6[10],"\x70\x6C\x61\x63\x65\x6D\x65\x6E\x74":_0xa6d6[11]});return false}}$(_0xa6d6[37])[_0xa6d6[36]](function(){var _0x8574x5=$(_0xa6d6[14])[_0xa6d6[13]]();var _0x8574x6=$(_0xa6d6[15])[_0xa6d6[13]]();var _0x8574x7=$(_0xa6d6[16])[_0xa6d6[13]]();var _0x8574x8=$(_0xa6d6[17])[_0xa6d6[13]]();var _0x8574x9=$(_0xa6d6[18])[_0xa6d6[13]]();var _0x8574xa=_0x8574x1(_0xa6d6[19]);var _0x8574xb=_0x8574x1(_0xa6d6[20]);var _0x8574xc=_0x8574x1(_0xa6d6[0]);var _0x8574xd=_0x8574x1(_0xa6d6[21]);var _0x8574xe=_0x8574x1(_0xa6d6[1]);if(!$(_0xa6d6[24])[_0xa6d6[23]](_0xa6d6[22])){$(_0xa6d6[25])[_0xa6d6[8]](_0xa6d6[5]);$(_0xa6d6[25])[_0xa6d6[12]]({"\x74\x72\x69\x67\x67\x65\x72":_0xa6d6[9],"\x74\x69\x74\x6C\x65":_0xa6d6[10],"\x70\x6C\x61\x63\x65\x6D\x65\x6E\x74":_0xa6d6[26]});return}else {$(_0xa6d6[25])[_0xa6d6[6]](_0xa6d6[5])};if(_0x8574xa&& _0x8574xb&& _0x8574xc&& _0x8574xd&& _0x8574xe){var _0x8574xf={name:_0x8574x5,last_name:_0x8574x6,mail:_0x8574x7,state:_0x8574x8,phone:_0x8574x9};$[_0xa6d6[34]]({type:_0xa6d6[27],url:_0xa6d6[28],data:_0x8574xf,success:function(_0x8574x10){if(_0x8574x10== _0xa6d6[29]){$(_0xa6d6[16])[_0xa6d6[8]](_0xa6d6[5]);$(_0xa6d6[16])[_0xa6d6[12]]({"\x74\x72\x69\x67\x67\x65\x72":_0xa6d6[9],"\x74\x69\x74\x6C\x65":_0xa6d6[30],"\x70\x6C\x61\x63\x65\x6D\x65\x6E\x74":_0xa6d6[11]})}else {$(_0xa6d6[16])[_0xa6d6[6]](_0xa6d6[5]);$(_0xa6d6[16])[_0xa6d6[12]]({"\x74\x72\x69\x67\x67\x65\x72":_0xa6d6[9],"\x74\x69\x74\x6C\x65":_0xa6d6[30],"\x70\x6C\x61\x63\x65\x6D\x65\x6E\x74":_0xa6d6[11]});$(_0xa6d6[33])[_0xa6d6[32]](_0xa6d6[31])}}})}else {if(!_0x8574xe){console[_0xa6d6[35]](_0xa6d6[1])}}});$(_0xa6d6[40])[_0xa6d6[36]](function(){location[_0xa6d6[38]]= _0xa6d6[39]});var _0x8574x11=[_0xa6d6[41],_0xa6d6[42],_0xa6d6[43],_0xa6d6[44],_0xa6d6[45],_0xa6d6[46],_0xa6d6[47],_0xa6d6[48],_0xa6d6[49],_0xa6d6[50],_0xa6d6[51],_0xa6d6[52],_0xa6d6[53],_0xa6d6[54],_0xa6d6[55],_0xa6d6[56],_0xa6d6[57],_0xa6d6[58],_0xa6d6[59],_0xa6d6[60],_0xa6d6[61],_0xa6d6[62],_0xa6d6[63],_0xa6d6[64],_0xa6d6[65],_0xa6d6[66],_0xa6d6[67],_0xa6d6[68],_0xa6d6[69],_0xa6d6[70],_0xa6d6[69],_0xa6d6[71],_0xa6d6[69],_0xa6d6[72],_0xa6d6[69],_0xa6d6[73],_0xa6d6[74],_0xa6d6[75],_0xa6d6[74],_0xa6d6[76],_0xa6d6[77],_0xa6d6[78],_0xa6d6[79],_0xa6d6[80],_0xa6d6[81],_0xa6d6[82],_0xa6d6[83],_0xa6d6[75],_0xa6d6[83],_0xa6d6[76],_0xa6d6[84],_0xa6d6[85],_0xa6d6[86],_0xa6d6[87],_0xa6d6[88],_0xa6d6[89],_0xa6d6[90],_0xa6d6[88],_0xa6d6[91],_0xa6d6[92]];for(var _0x8574x12=0;_0x8574x12< _0x8574x11[_0xa6d6[93]];_0x8574x12++){$(_0xa6d6[98])[_0xa6d6[97]](_0xa6d6[94]+ _0x8574x11[_0x8574x12]+ _0xa6d6[95]+ _0x8574x11[_0x8574x12]+ _0xa6d6[96])}})
+function validate(id) {
+    var regex = /^[a-zA-Z ]{2,30}$/;
+    if (id == "email") {
+        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    } else if (id == "phone") {
+        var regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    }
+    var ctrl = document.getElementById(id);
+    if (regex.test(ctrl.value)) {
+        $('#' + id).removeClass('wrong');
+        return true;
+    } else {
+        $('#' + id).addClass('wrong');
+        $(ctrl).tooltip({
+            'trigger': 'hover',
+            'title': 'Incorrect',
+            'placement': 'top'
+        });
+        return false;
+    }
+}
+$('button').click(function () {
+    var name = $('#name').val();
+    var last_name = $('#last_name').val();
+    var mail = $('#email').val();
+    var state = $('#state').val();
+    var phone = $('#phone').val();
+    var nameBool = validate("name");
+    var last_nameBool = validate("last_name");
+    var mailBool = validate("email");
+    var stateBool = validate("state");
+    var phoneBool = validate("phone");
+    if (!$('input[type="checkbox"]').is(':checked')) {
+        $('label').addClass('wrong');
+        $('label').tooltip({
+            'trigger': 'hover',
+            'title': 'Incorrect',
+            'placement': 'bottom'
+        });
+        return;
+    } else {
+        $('label').removeClass('wrong');
+    }
+    if (nameBool && last_nameBool && mailBool && stateBool && phoneBool) {
+        var data = {
+            name: name,
+            last_name: last_name,
+            mail: mail,
+            state: state,
+            phone: phone,
+        }
+        $.ajax({
+            type: 'POST',
+            url: '/register',
+            data: data,
+            success: function (res) {
+                if (res == '11000') {
+                    $('#email').addClass('wrong');
+                    $('#email').tooltip({
+                        'trigger': 'hover',
+                        'title': 'The e-mail already exists.',
+                        'placement': 'top'
+                    });
+                } else {
+                    $('#email').removeClass('wrong');
+                    $('#email').tooltip({
+                        'trigger': 'hover',
+                        'title': 'The e-mail already exists.',
+                        'placement': 'top'
+                    });
+                    $(".form-container").toggleClass('done');
+                }
+            }
+        });
+    } else if (!phoneBool) {
+        console.log('phone');
+    }
+});
+$('.top-nav > .logo').click(function () {
+    location.href = 'http://www.offcorss.com';
+})
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New", "Hampshire", "New", "Jersey", "New", "Mexico", "New", "York", "North", "Carolina", "North", "Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode", "Island", "South", "Carolina", "South", "Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West", "Virginia", "Wisconsin", "Wyoming"];
+for (var i = 0; i < states.length; i++) {
+    $('select').append('<option value="' + states[i] + '" class="col-xs-12">' + states[i] + '</option>');
+}
+});
